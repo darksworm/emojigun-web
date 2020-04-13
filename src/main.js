9c91dd053ee0950f86492a60db006e6d494a082f
ilmars <9987548+darksworm@users.noreply.github.com>
2020-04-13 19:07:18 +0300

    enable ip anonymization for GA #5

--- src/main.js
+++ src/main.js
@@ -1,22 +1,25 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import VueResource from "vue-resource";
-import VueAnalytics from "vue-analytics";
-import * as Sentry from "@sentry/browser";
-import * as Integrations from "@sentry/integrations";
-import config from "../config.json";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import VueResource from 'vue-resource';
+import VueAnalytics from 'vue-analytics';
+import * as Sentry from '@sentry/browser';
+import * as Integrations from '@sentry/integrations';
+import config from '../config.json';
 
-if (config.env === "production") {
+if (config.env === 'production') {
   Sentry.init({
     dsn: config.sentryDSN,
-    integrations: [new Integrations.Vue({ Vue, attachProps: true })]
+    integrations: [new Integrations.Vue({Vue, attachProps: true})],
   });
 
   Vue.use(VueAnalytics, {
     id: config.googleAnalyticsID,
-    router
+    router,
+    beforeFirstHit() {
+      Vue.$ga.set('anonymizeIp', true);
+    },
   });
 }
 
@@ -26,5 +29,5 @@ Vue.use(VueResource);
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app');
